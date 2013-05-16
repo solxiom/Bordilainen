c868904cd58216fbc39b1cf0d59b60dddcfb0981
Mohammad Bakharzy <mohammad.bakharzy@helsinki.fi>
2013-05-16 08:47:42 +0300

    sad

--- board/src/main/java/com/flatmates/board/service/BuildingComplexServiceImpl.java
+++ board/src/main/java/com/flatmates/board/service/BuildingComplexServiceImpl.java
@@ -4,6 +4,7 @@ import com.flatmates.board.domain.entity.BuildingComplex;
 import com.flatmates.board.domain.repository.BuildingComplexRepository;
 import com.flatmates.board.domain.service.BuildingComplexService;
 import java.util.Collection;
+import java.util.UUID;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,39 +12,43 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class BuildingComplexServiceImpl implements BuildingComplexService {
-	@Autowired
-	BuildingComplexRepository repo;
-	
-	@Override
-	public String createBuildingComplex(String address) {
-		/*
-		 * check for "new building" already exists in DB must be done here
-		 */
-		BuildingComplex building = new BuildingComplex();
-		building.setAddress(address);
-		repo.saveBuildingComplex(building);
-		return building.getId();
-	}
 
-	@Override
-	public void deleteBuildingComplex(BuildingComplex buildingComplex) {	
-		 repo.removeBuildingComplex(buildingComplex);
-	}
+    @Autowired
+    BuildingComplexRepository repo;
 
-	@Override
-	public Collection<BuildingComplex> queryByAddress(String address) {	
-		return repo.queryByAddress(address);
-	}
+    @Override
+    public String createBuildingComplex(String address) {
+        /*
+         * check for "new building" already exists in DB must be done here
+         */
+        BuildingComplex building = new BuildingComplex();
+        building.setAddress(address);
+        building.setId(UUID.randomUUID().toString());
+        if(repo == null){
+               System.out.println("454354354");
+        }
+        repo.saveBuildingComplex(building);
+        return building.getId();
+    }
 
-	@Override
-	public void updateBuildingComplexAddress(String building_id,String address) {
-		repo.updateBuildingComplexAddress(building_id, address);
+    @Override
+    public void deleteBuildingComplex(BuildingComplex buildingComplex) {
+        repo.removeBuildingComplex(buildingComplex);
+    }
 
-	}
+    @Override
+    public Collection<BuildingComplex> queryByAddress(String address) {
+        return repo.queryByAddress(address);
+    }
 
-	@Override
-	public Collection<BuildingComplex> listAll() {		
-		return repo.listAll();
-	}
+    @Override
+    public void updateBuildingComplexAddress(String building_id, String address) {
+        repo.updateBuildingComplexAddress(building_id, address);
 
+    }
+
+    @Override
+    public Collection<BuildingComplex> listAll() {
+        return repo.listAll();
+    }
 }
---
+++ board/src/test/java/com/flatmates/board/service/BuildingComplexServiceImplTest.java
@@ -0,0 +1,115 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.flatmates.board.service;
+
+import com.flatmates.board.domain.entity.BuildingComplex;
+import com.flatmates.board.domain.service.BuildingComplexService;
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.UUID;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author bakharzy
+ */
+public class BuildingComplexServiceImplTest {
+
+    BuildingComplexService buildingRepo = new BuildingComplexServiceImpl();
+
+    public BuildingComplexServiceImplTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of createBuildingComplex method, of class
+     * BuildingComplexServiceImpl.
+     */
+    @Test
+    public void testCreateBuildingComplex() {
+        System.out.println("createBuildingComplexTest");
+        String id = buildingRepo.createBuildingComplex("ida");
+        System.out.println("salam");
+        Collection<BuildingComplex> actualList = new LinkedList<BuildingComplex>();
+        System.out.println("----id: " + id);
+        actualList = buildingRepo.listAll();
+        assertFalse(actualList.isEmpty());
+    }
+
+    @Test
+    public void testDeleteBuildingComplex() {
+        System.out.println("deleteBuildingComplex");
+        BuildingComplex buildingComplex = null;
+        BuildingComplexServiceImpl instance = new BuildingComplexServiceImpl();
+        instance.deleteBuildingComplex(buildingComplex);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of queryByAddress method, of class BuildingComplexServiceImpl.
+     */
+    @Test
+    public void testQueryByAddress() {
+        System.out.println("queryByAddress");
+        String address = "";
+        BuildingComplexServiceImpl instance = new BuildingComplexServiceImpl();
+        Collection expResult = null;
+        Collection result = instance.queryByAddress(address);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of updateBuildingComplexAddress method, of class
+     * BuildingComplexServiceImpl.
+     */
+    @Test
+    public void testUpdateBuildingComplexAddress() {
+        System.out.println("updateBuildingComplexAddress");
+        String building_id = "";
+        String address = "";
+        BuildingComplexServiceImpl instance = new BuildingComplexServiceImpl();
+        instance.updateBuildingComplexAddress(building_id, address);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of listAll method, of class BuildingComplexServiceImpl.
+     */
+    @Test
+    public void testListAll() {
+        System.out.println("listAll");
+        BuildingComplexServiceImpl instance = new BuildingComplexServiceImpl();
+        Collection expResult = null;
+        Collection result = instance.listAll();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+}
