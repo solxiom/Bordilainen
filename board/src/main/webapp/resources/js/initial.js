1f3ce9440371aeb996c283728c80eb879d3b2c99
Kavan Soleimanbeigi <solxiom@gmail.com>
2013-05-20 20:08:39 +0300

    footer

--- board/src/main/webapp/resources/js/initial.js
+++ board/src/main/webapp/resources/js/initial.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
     console.log("Document is ready");
     try {
 
-            
+        
         $('#chooseBuilding').load("resources/html/buildings.html");
         // $(document.body).css('background','none');
         // $(document.body).css({'background':'url("/board/resources/img/bulletin_board2.jpg")','background-repeat': 'no-repeat'});
@@ -19,10 +19,11 @@ $(document).ready(function() {
         
         //$("#dialogMain").load("resources/html/dialog.html");
         //listBuildingStickers('7890');
-        $("#addButton").click(addStickers);
+//        $("#addButton").click(addStickers);
     } catch (e) {
         console.log("code emad rid" );
     }
+      loadFooter();
 
 });
 
@@ -45,4 +46,8 @@ function addhandlerToCombo(){
   
     });
   
+}
+function loadFooter(){
+    $('footer').load("/board/resources/html/footer.html");
+    
 }
